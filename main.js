50017963762ae1fea0bf7364106a9ea04056301d
JahanaSultan <cahana.aliyeva@gmail.com>
2022-07-19 16:10:42 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+const box=document.querySelector(".main")
+const select=document.getElementById('lang')
+
+const getinfo=(l)=>{
+box.innerHTML=''
+fetch(`https://api.currentsapi.services/v1/latest-news?language=${l}&apiKey=6P-DHWfKzvWGgieVHWKT_p-vjU0GwP89bFB4FJf50Ia391OY`)
+.then(res=>res.json())
+.then(data=>{
+data.news.map(e=>{
+    box.innerHTML+=''
+    box.innerHTML+=` <div class="card">
+<div class="photo">
+    <img src=${e.image} alt="">
+    <div class="category">${e.category[0]}</div>
+</div>
+<div class="content">
+    <h3><a href=${e.url} target="_blank"> ${e.title.substr(0,80)}</a></h3>
+    <p>${e.description.substr(0,70)}</p>
+    <div class="footer">
+    <p>${e.category.map(a=>`<p>#${a}</p>`)}</p>
+    </div>
+</div>
+</div>`}
+)
+})}
+
+getinfo('en')
+
+select.addEventListener("input",()=>{
+    getinfo(select.value)
+})
